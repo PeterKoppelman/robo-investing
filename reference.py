767fe2d2e1c89dace0c7a10222233e5953c1ae8e
PeterK <PeterKoppelman@users.noreply.github.com>
2020-07-13 11:06:50 -0400

    Add files via upload

---
+++ reference.py
@@ -0,0 +1,26 @@
+# reference.py - this is a reference file with 
+# sensitive data for the robo investing system.
+# Peter Koppelman - July 12, 2020
+
+# Email Password anc account
+password = 'NYUCapstone'
+account = 'capstone.roboinvesting@gmail.com'
+
+# Recipients of emails
+# recipients = ['pbkoppelman@gmail.com', 'michael.shore93@gmail.com']
+recipients = ['pbkoppelman@gmail.com']
+
+# Fastrack Investors appid and token
+# header = 'appid': "F075C6E1-759C-4009-9B47-5FE284F31F55",
+#    		 'token': "CD3266E5-7E3A-4150-A676-CF32B0F80167"
+
+# base portfolio
+# base_portfolio = [[55,30,5,5,5], [50,30,10,5,5], [45,30,10,10,5], [45,25,10,10,10], [40,25,15,10,10],
+# 	[40,20,15,15,10], [35,15,20,20,10], [30,15,25,20,10], [25,15,25,20,10], [25,15,30,25,5]]
+
+
+# adjuster for increase or decrease in risk
+# adj = .0125
+
+# security list
+# sec_list = ["VOO", "PRULX", "VTIAX", "PFORX", "FDHY", "SPY"]
