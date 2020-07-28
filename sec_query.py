3132091b7b3145f3541c8f6ba4f4cb4b38071dcd
michaelshore93 <33969704+michaelshore93@users.noreply.github.com>
2020-07-28 18:53:20 -0400

    Addressed threading issue.

--- sec_query.py
+++ sec_query.py
@@ -1,5 +1,5 @@
 import http.client
-from reference import appid, token
+from auth import appid, token
 import json
 from datetime import datetime
 from sqlalchemy import Column, Date, String, create_engine
@@ -8,21 +8,24 @@ from sqlalchemy.orm import sessionmaker
 import sqlite3
 from threading import Timer
 
-x=datetime.today()
-y=x.replace(day=x.day+1, hour=18, minute=45, second=0, microsecond=0)
-delta_t=y-x
+x = datetime.today()
+y = x.replace(day = x.day + 1, hour = 18, minute = 45, second = 0, microsecond = 0)
+delta_t = y - x
 
-secs=delta_t.seconds+1
+secs = delta_t.seconds+1
 
-engine = create_engine('sqlite:///roboinvest.db')
-Base = declarative_base()
-conn_sql = sqlite3.connect('roboinvest.db')
-c = conn_sql.cursor()
-now = str(datetime.now()).replace(':', '-').replace(' ', '-').replace('.', '-')
-timestamp = datetime.now()
+
 
 class Financial_Data:
+
     def ft_query():
+        engine = create_engine('sqlite:///roboinvest.db')
+        Base = declarative_base()
+        conn_sql = sqlite3.connect('roboinvest.db')
+        c = conn_sql.cursor()
+        now = str(datetime.now()).replace(':', '-').replace(' ', '-').replace('.', '-')
+        timestamp = datetime.now()
+
         conn = http.client.HTTPSConnection("ftlightning.fasttrack.net")
         headers = {
                 'appid': appid,
@@ -51,5 +54,5 @@ class Financial_Data:
                     pass
         c.close()
 
-t = Timer(secs, Financial_Data.ft_query())
+t = Timer(secs, Financial_Data.ft_query, args=())
 t.start()
